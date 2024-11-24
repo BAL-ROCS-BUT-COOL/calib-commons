595dec902f02e9bdb00f40dfbdf77de188103f93
“Tim Flueckiger <timflueckiger@outlook.com>
2024-11-24 13:17:46 +0100

    calib-commons to calib_commons

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='calib-commons',
+    name='calib_commons',
     version='0.1',
     packages=find_packages(),
     install_requires=[
